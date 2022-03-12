2fd3bb29dd1dcc5ce815873cd5e7569cebf3fd30
bernardogoltz <78585520+bernardogoltz@users.noreply.github.com>
2022-03-12 11:34:19 -0300

    modelo att
    criacao do modelo,
    taxa de acerto,
    loop de exibição,

--- dog-or-pig.py
+++ dog-or-pig.py
@@ -1,29 +1,45 @@
-# -*- coding: utf-8 -*-
+porco1 = [1, 1, 0]
+porco2 = [1, 1, 0]
+porco3 = [1, 1, 0]
+cachorro4 = [1, 1, 1]
+cachorro5 = [0, 1, 1]
+cachorro6 = [0, 1, 1]
 
-"""
-    1 - eh gordinho?
-    2 - perna curta?
-    3 - faz auau?
-"""
+dados = [porco1, porco2, porco3, cachorro4, cachorro5, cachorro6]
+
+marcacoes = [1, 1, 1, -1, -1, -1]
 
 from sklearn.naive_bayes import MultinomialNB
+
 modelo = MultinomialNB()
+modelo.fit(dados, marcacoes)
 
-porco1 = [1,1,0]
-porco2 = [1,1,0]
-porco3 = [1,0,0]
+misterioso1 = [1, 1, 1]
+misterioso2 = [1, 0, 0]
+misterioso3 = [1, 0, 1]
 
-cachorro1 = [1,1,1]
-cachorro2 = [0,1,1]
-cachorro3 = [0,1,1]
+teste = [misterioso1, misterioso2, misterioso3]
 
-dados =  [porco1 , porco2 , porco3,
-          cachorro1, cachorro2, cachorro3]
+marcacoes_teste = [-1, 1, 1]
 
-marcacoes = [1,1,1,-1,-1,-1]
+resultado = modelo.predict(teste)
 
-misterioso = [1,1,1]
 
-modelo.fit(dados , marcacoes)
-# tentarr se encaixar as informacoes dadas 
-# (x,y)
+diferencas = resultado - marcacoes_teste
+
+acertos = [d for d in diferencas if d == 0]
+
+total_de_acertos = len(acertos)
+total_de_elementos = len(teste)
+
+taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+
+idx = 1
+for x in resultado:
+    if x == 1:
+        print("{}) porco".format(idx))
+    else:
+        print("{}) cachorro".format(idx))
+    idx = idx + 1
+        
+print("Taxa de acerto = {:.2f}%".format(taxa_de_acerto))
